ef510a6c4f4e777920736f319ebadaf4c625a0b3
Владислав <voloshenko_vladislav@mail.ru>
2024-09-21 23:07:08 +0700

    fix login form position

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import { Navigate, useLocation } from 'react-router-dom';
 import { useLoginMutation } from '../store/AppService';
 import { useSelector } from 'react-redux';
 import { Button, Form, Input } from 'antd';
-
+import './login.scss';
 
 const LoginForm = () => {
   const [ login ] = useLoginMutation();
@@ -13,60 +13,62 @@ const LoginForm = () => {
   };
 
   return (
-    <Form
-      name='basic'
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
-      style={{
-        maxWidth: 600,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={handleSubmit}
-      autoComplete='off'
-    >
-      <Form.Item
-        label='Логин'
-        name='username'
-        rules={[
-          {
-            required: true,
-            message: 'Логин не должен быть пустым',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-
-      <Form.Item
-        label='Пароль'
-        name='password'
-        rules={[
-          {
-            required: true,
-            message: 'Пароль не должен быть пустым',
-          },
-        ]}
-      >
-        <Input.Password />
-      </Form.Item>
-
-      <Form.Item
+    <div className="login-form">
+      <Form
+        name='basic'
+        labelCol={{
+          span: 8,
+        }}
         wrapperCol={{
-          offset: 8,
           span: 16,
         }}
+        style={{
+          maxWidth: 600,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={handleSubmit}
+        autoComplete='off'
       >
-        <Button type='primary' htmlType='submit'>
-          Войти
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label='Логин'
+          name='username'
+          rules={[
+            {
+              required: true,
+              message: 'Логин не должен быть пустым',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label='Пароль'
+          name='password'
+          rules={[
+            {
+              required: true,
+              message: 'Пароль не должен быть пустым',
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type='primary' htmlType='submit'>
+            Войти
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
 };
 
